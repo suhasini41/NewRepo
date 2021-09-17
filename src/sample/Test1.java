e5162ad6e7b14aabc709bdbfe80b61f0d071f74c
suhasini41 <suhasini.mb@gmail.com>
2021-09-17 16:17:48 -0500

    committed new class files

---
+++ src/sample/Test1.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class Test1 
+{
+	public void diaplay()
+	{
+		System.out.println("I am Suhasini");
+	}
+
+}
---
+++ src/sample/Test2.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test1 ob = new Test1();
+		ob.diaplay();
+
+	}
+
+}
